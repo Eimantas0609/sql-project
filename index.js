f5b03e230b8b4259e276996daad884e317820d43
Eimantas0609 <100865215+Eimantas0609@users.noreply.github.com>
2022-08-02 10:24:41 +0300

    begin

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+import mysql from 'mysql2';
+
+const app = express();
+const port = 8081;
+app.use(express.static('public'));
+
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+app.get('/', (req, res) => res.render('index'));
+
+// DB start
+const connection = mysql.createConnection({
+    host: 'localhost',
+    database: 'classicmodels',
+    user: 'classicmodels',
+    password: 'bit'
+})
+
+app.get('/db', (req, res) => {
+    connection.execute('SELECT productLine, textDescription FROM productlines', (err, rows) => {
+        const data = rows.map(row => row);
+        res.render('db', {data: data});
+    });
+})
+//DB end
+
+app.listen(port, () => console.log(`Starting server on port ${port}`));
